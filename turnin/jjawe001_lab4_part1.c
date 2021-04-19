e0f46d2b16f0995f702051d1e6d50c4262e829f0
Jaired Jawed <jairedjawed@icloud.com>
2021-04-18 17:09:48 -0700

    Completed Part 1

--- turnin/jjawe001_lab4_part1.c
+++ turnin/jjawe001_lab4_part1.c
@@ -12,44 +12,63 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States { Init, S0, S1 } state;
+enum States { Start, B0Press, B0Release, B1Press, B1Release } state;
 
 void SM_Tick() {
 	switch (state) {
-		case Init:
-			state = S0;
+		case Start:
+			state = B0Press;
 			break;
-		case S0:
-			// If PINA0 is pressed, switch to s1
-			if (PINA == 0x01) {
-				state = S1;
+		case B0Press:
+			if ((PINA & 0x01) == 0x01) {
+				state = B0Press;
 			} 
 			else {
-				state = S0;
+				state = B0Release;
 			}
 			break;
-		case S1:
-			// if PINA1 is pressed, switch to S0
-			if (PINA == 0x01) {
-				state = S0;
+		case B0Release:
+			if ((PINA & 0x01) == 0x01) {
+				state = B1Press;
 			}
 			else {
-				state = S1;
+				state = B0Release;
+			}
+			break;
+		case B1Press:
+			if ((PINA & 0x01) == 0x01) {
+				state = B1Press;
+			}
+			else {
+				state = B1Release;
+			}
+			break;
+		case B1Release:
+			if ((PINA & 0x01) == 0x00) {
+				state = B0Press;
+			}
+			else {
+				state = B1Release;
 			}
 			break;
 		default:
-			state =  Init;
+			state = B0Press;
 			break;
 	}
 
 	switch (state) {
-		case Init:
-			break;
-		case S0:
+		case B0Press:
 			PORTB = 0x01;
 			break;
-		case S1:
+		case B0Release:
+			break;
+		case B1Press:
 			PORTB = 0x02;
+			break;
+		case B1Release:
+			break;
+		default:
+			PORTB = 0x01;
 			break;
 	}
 }
@@ -58,6 +77,8 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+
+    state = Start;
 
     /* Insert your solution below */
     while (1) {
